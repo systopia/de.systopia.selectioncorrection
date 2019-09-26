dead3174e9982da6a2feee129136a60662939103
B. Zschiedrich <zschiedrich@systopia.de>
2019-09-26 13:49:54 +0200

    Disable custom field filters where the custom field cannot be found.

--- CRM/Selectioncorrection/Filter/CustomFieldNotSet.php
+++ CRM/Selectioncorrection/Filter/CustomFieldNotSet.php
@@ -30,10 +30,35 @@ class CRM_Selectioncorrection_Filter_CustomFieldNotSet extends CRM_Selectioncorr
 
         parent::__construct();
 
-        $customFieldData = CRM_Selectioncorrection_CustomData::getCustomField($group, $field);
+        $customFieldData = [];
 
-        $this->tableName = $customFieldData['table_name'];
-        $this->columnName = $customFieldData['column_name'];
+        try
+        {
+            $customFieldData = CRM_Selectioncorrection_CustomData::getCustomField($group, $field);
+        }
+        catch (CiviCRM_API3_Exception $exception)
+        {
+            // If we get a CiviCRM_API3_Exception here, the custom field's group cannot be found.
+            // In this case, we set the customFieldData to an empty array, letting the filter know
+            // that its custom field is not available.
+            $customFieldData = [];
+        }
+
+        if (empty($customFieldData))
+        {
+            // If the customFieldData is empty, the custom field or its group cannot be found.
+            // In this case we deactivate the filter and make it non-optional, so the user can see
+            // that the filter is not available.
+            // TODO: Should we tell the user why?
+
+            $this->setStatus(false);
+            $this->optional = false;
+        }
+        else
+        {
+            $this->tableName = $customFieldData['table_name'];
+            $this->columnName = $customFieldData['column_name'];
+        }
     }
 
     /**
